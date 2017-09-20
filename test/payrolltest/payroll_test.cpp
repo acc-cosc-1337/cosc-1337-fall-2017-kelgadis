c135c5d806e5f5152db411a03b720a6597f09da0
kellyslane <31569784+kellyslane@users.noreply.github.com>
2017-09-20 18:59:31 -0500

    testing getOTHours()

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,1 +1,10 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+TEST(payrollTest, getOTHours_when_hours_less_than_40){
+    EXPECT_EQ(0, getOTHours(30));
+}
+
+TEST(ayrollTest, getOTHours_when_hours_greater_than_40){
+    EXPECT_EQ(10, getOTHours(50));
+}
