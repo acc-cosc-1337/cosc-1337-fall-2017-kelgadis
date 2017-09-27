262d3449137444d0521c5d3251c62db52eb30170
kellyslane <31569784+kellyslane@users.noreply.github.com>
2017-09-27 18:29:42 -0500

    unit test 1 for makeNumber

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -5,6 +5,11 @@ TEST(payrollTest, getOTHours_when_hours_less_than_40){
     EXPECT_EQ(0, getOTHours(30));
 }
 
-TEST(ayrollTest, getOTHours_when_hours_greater_than_40){
+TEST(payrollTest, getOTHours_when_hours_greater_than_40){
     EXPECT_EQ(10, getOTHours(50));
 }
+
+TEST(payrollTest, makeNumber_when_sent_string_including_letters){
+    EXPECT_EQ(-1, makeNumber("12dt5"));
+}
+
