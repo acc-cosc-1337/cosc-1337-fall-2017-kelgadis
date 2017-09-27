cb310e9f281943f31a28aeddfb31bcedd6938511
kellyslane <31569784+kellyslane@users.noreply.github.com>
2017-09-27 18:30:29 -0500

    unit test 2 for makeNumber

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -13,3 +13,7 @@ TEST(payrollTest, makeNumber_when_sent_string_including_letters){
     EXPECT_EQ(-1, makeNumber("12dt5"));
 }
 
+TEST(payrollTest, makeNumber_when_sent_string_of_numbers_only){
+    EXPECT_EQ(12345, makeNumber("12345"));
+}
+
