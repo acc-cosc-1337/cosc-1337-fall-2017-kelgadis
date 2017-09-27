8dc6c7c5f84dd1ccebbc70dca4aac2365eee1778
kellyslane <31569784+kellyslane@users.noreply.github.com>
2017-09-27 18:25:23 -0500

    new payroll functions

--- src/Payroll.cpp
+++ src/Payroll.cpp
@@ -1,167 +1,142 @@
 #include "Payroll.h"
-#include <math.h>
 #include <iostream>
 #include <string>
-#include <sstream> // storing streams to strings
+#include <sstream>
 #include <iomanip>
-#include <cctype>
+#include <vector>
+
 using namespace std;
 
-bool isNumber(double input)
+const double STANDARD_HOURS = 40.0;
+const double BIWEEKLY = 26.0;
+const double FIT = 0.15;
+const double FICASSN = 0.062;
+const double FICAMED = 0.0145;
+
+/*
+	Prints error messages
+
+	@param option is type of error
+	@param range1 is lower range limit
+	@param range2 is upper range limit
+*/
+
+void printErrorMessage(string option, double range1, double range2)
 {
-	if (cin.fail())
+	if (option == "Number")
 	{
-		cin.clear();
-		cin.ignore(123456789, '\n');
 		cout << "~~~Please use numbers only.~~~" << endl;
-		return false;
 	}
-	else return true;
+	else if (option == "Letter")
+	{
+		cout << "~~~Not an option, please try again.~~~" << endl;
+	}
+	else if (option == "Range")
+	{
+		cout << "~~~Out of range, please enter a number between " << range1 << " and " << range2 << ".~~~" << endl;
+	}
 }
 
-bool isInRange(double input, double range1, double range2)
+/*
+	Converts string user input to number and assures no letters were entered
+
+	@param userInput is input entered by user
+
+	@returns number
+*/
+
+double makeNumber(string userInput)
+{
+	double number = 0.0;
+	for (int i = 0; i < userInput.size(); i++)
+	{
+		if (isalpha(userInput[i]))
+		{
+			number = -1.0;
+			return number;
+		}
+	}
+	number = stod(userInput);
+	return number;
+}
+
+/*
+	Gets ASCII value for employee type & verifies good input
+
+	@param userInput is what the user entered
+	@param letter1 is first option required
+	@param letter2 is second option required
+
+	@returns employee type
+*/
+
+int getEmployeeType(string userInput, char letter1, char letter2)
+{
+	int employee = 0;
+	for (int i = 0; i < userInput.size(); i++)
+	{
+		if (!isalpha(userInput[i]))
+		{
+			employee = -1;
+		}
+	}
+	char converter = toupper(userInput[0]);
+	if (converter != letter1 && converter != letter2)
+	{
+		employee = -1;
+	}
+	else if (converter == letter1 || converter == letter2)
+	{
+		employee = int(converter);
+	}
+	return employee;
+}
+
+/*
+	Ensures input entered is within range required
+
+	@param input is number entered by user
+	@param range1 is lower limit
+	@param range2 is upper limit
+
+	returns input
+*/
+
+double isInRange(double input, double range1, double range2)
 {
 	if (input < range1 || input > range2)
 	{
-		cout << "~~~Out of range, please enter a number between " << range1 << " and " << range2 << ".~~~" << endl;
-		return false;
+		input = -1;
 	}
-	else return true;
+	return input;
 }
 
-string getEmployeeName(int employee)
-{
-	string firstName, lastName;
-	if (employee == 1)
-	{
-		cout << "\nFirst Employee:" << endl;
-	}
-	else if (employee == 2)
-	{
-		cout << "\nSecond Employee:" << endl;
-	}
-	else
-	{
-		cout << "\nThird Employee:" << endl;
-	}
-	cout << "1) Enter employee's first name: ";
-	cin >> firstName;
-	cout << "2) Enter employee's last name: ";
-	cin >> lastName;
+/*
+	Calculates overtime hours
 
-	string fullName = firstName + " " + lastName;
-	return fullName;
-}
+	@param hours is hours worked in one week
 
-bool isLetter(string input)
-{
-	bool returnValue = false;
-	char letter = input[0];
-	if (isalpha(letter))
-	{
-		returnValue = true;
-	}
-	else
-	{
-		cout << "~~~Not an option, please try again.~~~" << endl;
-		returnValue = false;
-	}
-	return returnValue;
-}
-
-string getEmployeeType()
-{
-	char input = 'a';
-	string employeeType = " ";
-	do
-	{
-		bool checkInput = false;
-		while (checkInput == false)
-		{
-			cout << "3) Enter type of employee, Hourly or Salary: ";
-			cin >> employeeType;
-			checkInput = isLetter(employeeType);
-		}
-		input = toupper(employeeType[0]);
-		if (input == 'H')
-		{
-			employeeType = "Hourly";
-		}
-		else if (input == 'S')
-		{
-			employeeType = "Salary";
-		}
-		else cout << "~~~Not an option, please try again.~~~" << endl;
-	} while (input != 'H' && input != 'S');
-	return employeeType;
-}
-
-double getRegularHours()
-{
-	bool checkRange = false;
-	double hours = 0.0;
-	do
-	{
-		bool checkInput = false;
-		while (checkInput == false)
-		{
-			cout << "4) Enter total hours worked: ";
-			cin >> hours;
-
-			checkInput = isNumber(hours);
-		}
-		checkRange = isInRange(hours, 0.0, 60.0);
-	} while (checkRange == false);
-	return hours;
-}
+	@returns overtime hours worked in one week
+*/
 
 double getOTHours(double hours)
 {
 	double otHours = 0.0;
-	if (hours > 40.0)
+	if (hours > STANDARD_HOURS)
 	{
-		otHours = hours - 40.0;
+		otHours = hours - STANDARD_HOURS;
 	}
 	return otHours;
 }
 
-double getPayRate()
-{
-	bool checkRange = false;
-	double payRate = 0.0;
-	do
-	{
-		bool checkInput = false;
-		while (checkInput == false)
-		{
-			cout << "5) Enter hourly pay rate: ";
-			cin >> payRate;
+/*
+	Calculates overtime pay for hourly employees
 
-			checkInput = isNumber(payRate);
-		}
-		checkRange = isInRange(payRate, 0.0, 50.0);
-	} while (checkRange == false);
-	return payRate;
-}
+	@param otHours is overtime hours worked in one week
+	@param otPayRate is overtime hourly pay rate
 
-double getOTPayRate()
-{
-	bool checkRange = false;
-	double otPayRate = 0.0;
-	do
-	{
-		bool checkInput = false;
-		while (checkInput == false)
-		{
-			cout << "6) Enter overtime hourly pay rate: ";
-			cin >> otPayRate;
-
-			checkInput = isNumber(otPayRate);
-		}
-		checkRange = isInRange(otPayRate, 0.0, 75.0);
-	} while (checkRange == false);
-	return otPayRate;
-}
+	@returns overtime pay
+*/
 
 double getOTPay(double otHours, double otPayRate)
 {
@@ -169,61 +144,102 @@ double getOTPay(double otHours, double otPayRate)
 	return otPay;
 }
 
-double getRegularPay(string employee, double hours, double payRate)
+/*
+	Calculates regular pay for hourly employees
+
+	@param hours is hours worked in one week
+	@param payRate is hourly pay rate
+
+	@returns regular pay
+*/
+
+double getRegularPay(double hours, double payRate)
 {
 	double regularPay = hours * payRate;
 	return regularPay;
 }
 
-double getRegularPay(string employee)
-{
-	bool checkRange = false;
-	double regularPay = 0.0;
-	do
-	{
-		bool checkInput = false;
-		while (checkInput == false)
-		{
-			cout << "4) Enter yearly salary: ";
-			cin >> regularPay;
+/*
+	Calculates gross pay for hourly employees
 
-			checkInput = isNumber(regularPay);
-		}
-		checkRange = isInRange(regularPay, 20000.0, 100000.0);
-		break;
-	} while (checkRange == false);
-	return regularPay;
-}
+	@param regularPay is pay earned in one week for hourly employees
+	@param otPay is overtime pay earned in one week for hourly employees
 
-double getGrossPay(double regularPay, double otPay) // hourly
+	@returns gross pay
+*/
+
+double getGrossPay(double regularPay, double otPay)
 {
 	double grossPay = regularPay + otPay;
 	return grossPay;
 }
 
-double getGrossPay(double regularPay) // salary
+/*
+	Calculates gross pay for salary employees
+
+	@param regularPay is yearly salary for salary employees
+	
+	@returns gross pay
+*/
+
+double getGrossPay(double regularPay)
 {
-	double grossPay = regularPay / 26;
+	double grossPay = regularPay / BIWEEKLY;
 	return grossPay;
 }
 
+/*
+	Calculates federal income tax deduction
+
+	@param grossPay is gross pay for either type of employee
+
+	@returns federal income tax deduction
+*/
+
 double getFIT(double grossPay)
 {
-	double fedIncomeTax = 0.15 * grossPay;
+	double fedIncomeTax = FIT * grossPay;
 	return fedIncomeTax;
 }
 
+/*
+	Calculates FICA Social Security deduction
+
+	@param grossPay is gross pay for either type of employee
+
+	@returns FICA Social Security deduction
+*/
+
 double getFICASSN(double grossPay)
 {
-	double ficaSSN = 0.062 * grossPay;
+	double ficaSSN = FICASSN * grossPay;
 	return ficaSSN;
 }
 
+/*
+	Calculates FICA Medicare deduction
+
+	@param grossPay is gross pay for either type of employee
+
+	@returns FICA Medicare deduction
+*/
+
 double getFICAMED(double grossPay)
 {
-	double ficaMED = 0.0145 * grossPay;
+	double ficaMED = FICAMED * grossPay;
 	return ficaMED;
 }
+
+/*
+	Calculates net pay
+
+	@param grossPay is gross pay for either type of employee
+	@param fedIncomeTax is federal income tax deduction
+	@param ficaSSN is FICA Social Security deduction
+	@param ficaMED is FICA Medicare deduction
+
+	@returns net pay
+*/
 
 double getNetPay(double grossPay, double fedIncomeTax, double ficaSSN, double ficaMED)
 {
@@ -231,39 +247,49 @@ double getNetPay(double grossPay, double fedIncomeTax, double ficaSSN, double fi
 	return netPay;
 }
 
-string printHeaderLine()
+/*
+	Prints title bar for employee data output
+
+	@returns title bar as string
+*/
+
+string printTitleBar()
 {
-	// setw() constants:
-	const int nameWidth = 17;
-	const int normalWidth = 13;
-	const int largeWidth = 20;
-	const string columnLine = " |";
+	const int name = 17;
+	const int normal = 13;
+	const int large = 20;
+	const string column = " |";
 
-	ostringstream headerLine;
+	ostringstream titleBar;
 
-	// title bar
-	headerLine << columnLine
-			   << left << setw(nameWidth) << "Name" << columnLine
-			   << right << setw(normalWidth) << "Hours" << columnLine
-			   << right << setw(normalWidth) << "OT Hours" << columnLine
-			   << right << setw(normalWidth) << "Hourly Rate" << columnLine
-			   << right << setw(normalWidth) << "OT Rate" << columnLine
-			   << right << setw(normalWidth) << "Regular Pay" << columnLine
-			   << right << setw(normalWidth) << "OT Pay" << columnLine
-			   << right << setw(normalWidth) << "Gross Pay" << columnLine
-			   << right << setw(normalWidth) << "FICA SSN" << columnLine
-			   << right << setw(largeWidth) << "FICA Medicare" << columnLine
-			   << right << setw(largeWidth) << "Federal Income Tax" << columnLine
-			   << right << setw(normalWidth) << "Net Pay" << columnLine << endl;
+	titleBar << column
+		<< left << setw(name) << "Name" << column
+		<< right << setw(normal) << "Hours" << column
+		<< right << setw(normal) << "OT Hours" << column
+		<< right << setw(normal) << "Hourly Rate" << column
+		<< right << setw(normal) << "OT Rate" << column
+		<< right << setw(normal) << "Regular Pay" << column
+		<< right << setw(normal) << "OT Pay" << column
+		<< right << setw(normal) << "Gross Pay" << column
+		<< right << setw(normal) << "FICA SS" << column
+		<< right << setw(large) << "FICA Medicare" << column
+		<< right << setw(large) << "Federal Income Tax" << column
+		<< right << setw(normal) << "Net Pay" << column << endl;
 
-	return headerLine.str();
+	return titleBar.str();
 }
+
+/*
+	Prints separator line
+
+	@returns dashed line as string
+*/
 
 string printLineOfDashes(char position)
 {
 	ostringstream dashes;
 
-	if (position == 'T' || position == 'B') // if top or bottom
+	if (position == 'T' || position == 'B')
 	{
 		dashes << " " << string(199, '-') << endl;
 	}
@@ -274,67 +300,63 @@ string printLineOfDashes(char position)
 	return dashes.str();
 }
 
-string addDollarSign(double input)
+/*
+	Converts money type data to strings and prints all employee data to screen
+*/
+
+void printEmployeeData(int employee, string fullName, double hours, double otHours, double payRate, double otRate, double regularPay,
+					   double otPay, double grossPay, double ficaSSN, double ficaMED, double fedIncomeTax, double netPay)
 {
-	ostringstream makeItMoney;
+	enum employeeType { Hourly = 72, Salary = 83 };
+	const int name = 17;
+	const int normal = 13;
+	const int large = 20;
+	const string column = " |";
 
-	makeItMoney << fixed << setprecision(2) << "$" << input;
+	ostringstream pr, otr, rp, otp, gp, fssn, fmed, fit, np;
+	pr << fixed << setprecision(2) << "$" << payRate;
+	otr << fixed << setprecision(2) << "$" << otRate;
+	rp << fixed << setprecision(2) << "$" << regularPay;
+	otp << fixed << setprecision(2) << "$" << otPay;
+	gp << fixed << setprecision(2) << "$" << grossPay;
+	fssn << fixed << setprecision(2) << "$" << ficaSSN;
+	fmed << fixed << setprecision(2) << "$" << ficaMED;
+	fit << fixed << setprecision(2) << "$" << fedIncomeTax;
+	np << fixed << setprecision(2) << "$" << netPay;
 
-	return makeItMoney.str();
+	string stringPayRate, stringOTRate, stringRegularPay, stringOTPay, stringGrossPay, stringFSSN, stringFMED, stringFIT, stringNetPay;
+	stringPayRate = pr.str();
+	stringOTRate = otr.str();
+	stringRegularPay = rp.str();
+	stringOTPay = otp.str();
+	stringGrossPay = gp.str();
+	stringFSSN = fssn.str();
+	stringFMED = fmed.str();
+	stringFIT = fit.str();
+	stringNetPay = np.str();
+
+	cout << column << left << setw(name) << fullName << column;
+	if (employee == Salary)
+	{
+		cout << right << setw(normal) << "n/a" << column
+			 << right << setw(normal) << "n/a" << column
+			 << right << setw(normal) << "n/a" << column
+			 << right << setw(normal) << "n/a" << column 
+			 << right << setw(normal) << stringRegularPay << column
+			 << right << setw(normal) << "n/a" << column;
+	}
+	else
+	{
+		cout << right << setw(normal) << fixed << setprecision(2) << hours << column
+			 << right << setw(normal) << fixed << setprecision(2) << otHours << column
+			 << right << setw(normal) << stringPayRate << column
+			 << right << setw(normal) << stringOTRate << column
+			 << right << setw(normal) << stringRegularPay << column
+			 << right << setw(normal) << stringOTPay << column;
+	}
+	cout << right << setw(normal) << stringGrossPay << column
+		 << right << setw(normal) << stringFSSN << column
+		 << right << setw(large) << stringFMED << column
+		 << right << setw(large) << stringFIT << column
+		 << right << setw(normal) << stringNetPay << column << endl;
 }
-
-string storePayrollHourly(string employee, string fullName, double hours, double otHours, double payRate, double otRate, double regularPay,
-						  double otPay, double grossPay, double ficaSSN, double ficaMED, double fedIncomeTax, double netPay)
-{
-	// setw() constants:
-	const int nameWidth = 17;
-	const int normalWidth = 13;
-	const int largeWidth = 20;
-	const string columnLine = " |";
-
-	ostringstream employeeData;
-
-	// employee data
-	employeeData << columnLine
-				 << left << setw(nameWidth)  << fullName << columnLine
-				 << right << setw(normalWidth) << fixed << setprecision(2) << hours << columnLine
-				 << right << setw(normalWidth) << fixed << setprecision(2) << otHours << columnLine
-				 << right << setw(normalWidth) << addDollarSign(payRate) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(otRate) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(regularPay) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(otPay) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(grossPay) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(ficaSSN) << columnLine
-				 << right << setw(largeWidth) << addDollarSign(ficaMED) << columnLine
-				 << right << setw(largeWidth) << addDollarSign(fedIncomeTax) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(netPay) << columnLine << endl;
-
-	return employeeData.str();
-}
-
-string storePayrollSalary(string employee, string fullName, double regularPay, double grossPay, double ficaSSN, double ficaMED, double fedIncomeTax, double netPay)
-{
-	const int nameWidth = 17;
-	const int normalWidth = 13;
-	const int largeWidth = 20;
-	const string columnLine = " |";
-
-	ostringstream employeeData;
-
-	employeeData << columnLine
-				 << left << setw(nameWidth) << fullName << columnLine
-				 << right << setw(normalWidth) << "n/a" << columnLine
-				 << right << setw(normalWidth) << "n/a" << columnLine
-				 << right << setw(normalWidth) << "n/a" << columnLine
-				 << right << setw(normalWidth) << "n/a" << columnLine
-				 << right << setw(normalWidth) << addDollarSign(regularPay) << columnLine
-				 << right << setw(normalWidth) << "n/a" << columnLine
-				 << right << setw(normalWidth) << addDollarSign(grossPay) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(ficaSSN) << columnLine
-				 << right << setw(largeWidth) << addDollarSign(ficaMED) << columnLine
-				 << right << setw(largeWidth) << addDollarSign(fedIncomeTax) << columnLine
-				 << right << setw(normalWidth) << addDollarSign(netPay) << columnLine << endl;
-
-	return employeeData.str();
-}
-
