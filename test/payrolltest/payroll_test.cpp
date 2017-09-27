3628f361a6e3e62ef9e633c7235cb36d58d0f7ee
kellyslane <31569784+kellyslane@users.noreply.github.com>
2017-09-27 18:31:44 -0500

    changed ints to doubles

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -2,18 +2,18 @@
 #include "Payroll.h"
 
 TEST(payrollTest, getOTHours_when_hours_less_than_40){
-    EXPECT_EQ(0, getOTHours(30));
+    EXPECT_EQ(0.0, getOTHours(30.0));
 }
 
 TEST(payrollTest, getOTHours_when_hours_greater_than_40){
-    EXPECT_EQ(10, getOTHours(50));
+    EXPECT_EQ(10.0, getOTHours(50.0));
 }
 
 TEST(payrollTest, makeNumber_when_sent_string_including_letters){
-    EXPECT_EQ(-1, makeNumber("12dt5"));
+    EXPECT_EQ(-1.0, makeNumber("12dt5"));
 }
 
 TEST(payrollTest, makeNumber_when_sent_string_of_numbers_only){
-    EXPECT_EQ(12345, makeNumber("12345"));
+    EXPECT_EQ(12345.0, makeNumber("12345"));
 }
 
